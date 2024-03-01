197a8e0577b917e441238e02e65b77347f8d3a83
PUN <punnawat.mungkalarungsi@gmail.com>
2024-03-01 23:51:16 +0700

    Refactor tree insertion functions and add parameter for tree size

--- Tree/LAB/Lab6_1.c
+++ Tree/LAB/Lab6_1.c
@@ -5,113 +5,107 @@
 
 #define MAX_SIZE 1000
 
-// Structure to represent a node in the binary tree
-typedef struct {
-    int data;
-} Node;
-
-// Array to store the binary tree
-Node tree[MAX_SIZE];
-int treeSize = 0; // Variable to hold the size of the tree
-
-void insertRoot(int data) {
-    if (tree[0].data != -999) {
+void insertRoot(int tree[], int treeSize, int data) {
+    if (tree[0] != -999) {
         printf("cannot create duplicated root\n");
         return;
     }
-    tree[0].data = data;
+    tree[0] = data;
 }
 
-// Function to insert a node on the left
-void insertLeft(int parentIndex, int data) {
+void insertLeft(int tree[], int treeSize, int parentIndex, int data) {
     int leftChildIndex = 2 * parentIndex + 1;
     if (leftChildIndex >= treeSize) {
         printf("overflow\n");
         return;
     }
-    if (tree[leftChildIndex].data != -999) {
+    if (tree[leftChildIndex] != -999) {
          printf("node already presented\n");
     }
     else {
-        tree[leftChildIndex].data = data;
+        tree[leftChildIndex] = data;
     }
 }
 
-// Function to insert a node on the right
-void insertRight(int parentIndex, int data) {
+void insertRight(int tree[], int treeSize, int parentIndex, int data) {
     int rightChildIndex = 2 * parentIndex + 2;
     if (rightChildIndex >= treeSize) {
         printf("overflow\n");
         return;
     }
-    if (tree[rightChildIndex].data != -999) {
+    if (tree[rightChildIndex] != -999) {
          printf("node already presented\n");
     }
     else {
-        tree[rightChildIndex].data = data;
+        tree[rightChildIndex] = data;
     }
 }
 
-// Function to find a node with given data
-int findNode(int data) {
+int findNode(int tree[], int treeSize, int data) {
     for (int i = 0; i < treeSize; i++) {
-        if (tree[i].data == data) {
+        if (tree[i] == data) {
             return i;
         }
     }
     return -1;
 }
 
-void printTree() {
+void printTree(int tree[], int treeSize) {
     for (int i = 0; i < treeSize; i++) {
-        if (tree[i].data != -999)
-            printf("%d ", tree[i].data);
+        if (tree[i] != -999)
+            printf("%d ", tree[i]);
         else
             printf("null ");
     }
 }
 
 int main() {
+    int treeSize;
     scanf("%d", &treeSize);
+    int tree[MAX_SIZE];
+    for (int i = 0; i < MAX_SIZE; i++) {
+        tree[i] = -999; // Initialize tree nodes
+    }
+
     int data = -999, root = 0;
     for (int i = 0; i < treeSize; i++) {
         scanf("%d", &data);
-        tree[i].data = data;
+        tree[i] = data;
     }
     char opt[5];
     while (1)
     {
-        scanf(" %s", &opt);
+        scanf(" %s", opt);
         if (strcmp(opt, "ROOT") == 0)
         {
             scanf("%d", &data);
-            insertRoot(data);
+            insertRoot(tree, treeSize, data);
         }
         else if (strcmp(opt, "INSL") == 0)
         {
             scanf("%d", &root);
             scanf("%d", &data);
-            int index = findNode(root);
+            int index = findNode(tree, treeSize, root);
             if (index == -1) {
                 printf("not found\n");
             } else {
-                insertLeft(index, data);
+                insertLeft(tree, treeSize, index, data);
             }
         }
         else if (strcmp(opt, "INSR") == 0)
         {
             scanf("%d", &root);
             scanf("%d", &data);
-            int index = findNode(root);
+            int index = findNode(tree, treeSize, root);
             if (index == -1) {
                 printf("not found\n");
             } else {
-                insertRight(index, data);
+                insertRight(tree, treeSize, index, data);
             }
         }
         else if (strcmp(opt, "END") == 0)
         {
-            printTree();
+            printTree(tree, treeSize);
             break;
         }
     }
